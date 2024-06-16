276774b5eb7e8718cd2a2f410adad77f34a2f6c1
FirasJaballi <firas.jaballi@esprit.tn>
2024-06-16 21:33:26 +0100

    extracting each line

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <stdint.h>
 #include <fcntl.h>
@@ -15,11 +16,29 @@ ssize_t read_content(const char *const file_name, char **const buff) {
   return size;
 }
 
-int main(int argc, char **argv) {
-  char *file_name="sample.csv";
+void lex_line(const char *const line) {
+  printf("%s\n", line);
+}
+
+void lex(const char *const file_name) {
   char *content=0;
   ssize_t read_bytes=read_content(file_name, &content);
-  printf("%s\n", content);
+  const char *const delim=",", *const line_break="\n";
+  char *start=content, *const end=content+read_bytes;
+  char *match=0;
+  char line[1000]={0};
+  while(start != end) {
+    match=strstr(start, line_break);
+    memcpy(line, start, match-start);
+    line[match-start]='\0';
+    lex_line(line);
+    start=match+1;
+  }
   free(content);
+}
+
+int main(int argc, char **argv) {
+  char *file_name="sample.csv";
+  lex(file_name);
   return EXIT_SUCCESS;
 }
